7d6fda7788d41741253e49d2ff7e6df659c7c649
divrawat <divrawat2001@gmail>
2024-07-02 21:51:20 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,687 @@
+
+// export const DOMAIN = 'http://localhost:3000';
+export const DOMAIN = 'https://www.thegreatestestatedeveloper.in.net';
+
+export const MANGA_DESCRIPTION = `<a href="${DOMAIN}"><u>The Greatest Estate Developer</u></a> is a captivating novel that follows the journey of a visionary protagonist who rises to prominence in the cutthroat world of real estate development. Driven by ambition and a keen business acumen, they navigate through challenges and rivalries, aiming to build the most impressive estates and reshape the skyline of their city.`;
+
+export const MANGA_NAME = 'The Greatest Estate Developer';
+export const MANGA_AUTHOR = 'BK_Moon';
+export const MANGA_RELEASE = '2022';
+export const MANGA_STATUS = 'Ongoing';
+export const MANGA_ARTIST = 'Kim Hyunsoo';
+export const MANGA_STUDIO = 'None';
+export const MANGA_GENRE = 'Webtoon, Fantasy, Comedy, Drama';
+
+export const CHAPTER_PREFIX = 'the-greatest-estate-developer-chapter';
+export const IMAGE_PREFIX = 'images/the-greatest-estate-developer';
+export const NEXT_PREVIOUS_PREFIX = 'manga/the-greatest-estate-developer-chapter';
+export const URL_PREFIX = 'manga/the-greatest-estate-developer-chapter';
+
+export const AUTHOR_PAGE = `${DOMAIN}/the-greatest-estate-developer-team`;
+export const LOGO_URL = `${DOMAIN}/logo.webp`;
+export const COVER_IMG = `${DOMAIN}/cover.webp`;
+export const BEHIND_COVER_IMG = "https://www.ascomaxx.com/uploads/large/847d2469a52fc9313bda21e4ff7f0c22.jpg";
+
+
+// Navbar
+export const logo = <img src={`${LOGO_URL}`} alt="Logo" className="h-[75px] w-[75px] rounded-full mr-5 md:ml-0 ml-4 md:pb-2 md:my-0 my-1" />
+
+export const APP_NAME = 'The Greatest Estate Developer';
+export const NavbarName = "The Greatest ED";
+
+export const APP_DESCRIPTION = `Read ${MANGA_NAME} manga online at ${DOMAIN}. It follows the journey of a visionary protagonist who rises to prominence in the cutthroat world of real estate development. Driven by ambition and a keen business acumen, they navigate through challenges and rivalries, aiming to build the most impressive estates and reshape the skyline of their city.`;
+
+export const DISQUS_SHORTNAME = "my-cms-7";
+
+
+export const MANGA_SUMMARY = [
+    {
+        id: 1,
+        content: "The Greatest Estate Developer unfolds as a compelling narrative centered around a protagonist whose journey from obscurity to prominence mirrors the ambitious world of real estate development. Starting with humble origins or inheriting a modest foundation, the character emerges as a visionary force within the industry, driven by a passion to reshape urban landscapes and redefine architectural standards."
+    },
+    {
+        id: 2,
+        content: "From the outset, the novel immerses readers in the protagonist's strategic mindset and entrepreneurial spirit. They possess an innate ability to spot lucrative opportunities and forge strategic alliances, leveraging these talents to catapult themselves to the forefront of their field. Their rise is marked by a series of pivotal projects that not only showcase their creative vision but also cement their reputation as a trailblazer in the competitive world of estate development."
+    },
+    {
+        id: 3,
+        content: "Central to the character's journey is their ability to navigate a landscape fraught with challenges and rivalries. Whether it's overcoming bureaucratic hurdles, navigating complex zoning regulations, or negotiating with formidable stakeholders, the protagonist's resilience and determination are consistently tested. Each obstacle serves as a catalyst for growth, pushing them to innovate and adapt their strategies while maintaining a steadfast commitment to their overarching goals."
+    },
+    {
+        id: 4,
+        content: "Beyond their professional achievements, the protagonist is portrayed with depth and complexity, grappling with personal sacrifices and ethical dilemmas that accompany their ascent to success. The novel delves into their relationships with family, friends, and colleagues, revealing the human side of their ambition and the impact of their decisions on those around them."
+    },
+    {
+        id: 5,
+        content: "As the narrative unfolds, readers witness the protagonist's evolution from an ambitious dreamer to a transformative figure within the industry. Their projects not only redefine city skylines but also resonate with broader themes of sustainability, community engagement, and architectural innovation. Each development becomes a testament to their bold vision and enduring legacy, shaping the fabric of urban life and leaving a lasting imprint on future generations."
+    },
+    {
+        id: 6,
+        content: "The Greatest Estate Developer is a rich tapestry of ambition, perseverance, and the relentless pursuit of excellence. It invites readers into a world where creativity meets commerce, where the protagonist's journey serves as both a reflection of contemporary urban development and a timeless exploration of human ambition in pursuit of greatness."
+    }
+];
+
+
+
+
+export const navLinks = [
+    { text: 'Home', href: `${DOMAIN}` },
+    { text: 'DMCA', href: `${DOMAIN}/dmca` },
+    { text: 'Terms & Conditions', href: `${DOMAIN}/terms-and-conditions` },
+];
+
+export const FooterLinks = [
+    { text: 'About', href: `${DOMAIN}/about` },
+    { text: 'Contact', href: `${DOMAIN}/contact` },
+    { text: 'Disclaimer', href: `${DOMAIN}/disclaimer` },
+    { text: 'Privacy Policy', href: `${DOMAIN}/privacy-policy` },
+    { text: 'Terms & Conditions', href: `${DOMAIN}/terms-and-conditions` }
+];
+
+
+
+export const RelatedMangaLinks = [
+    /*
+    {
+        imageUrl: `${DOMAIN}/related/frieran.webp`,
+        title: 'Frieran Manga',
+        link: 'https://www.frierenmanga.in.net',
+    },
+    */
+
+];
+
+
+
+
+
+export const chaptersData = [
+    {
+        "chapterNumber": "1",
+        "numImages": 18
+    },
+    {
+        "chapterNumber": "2",
+        "numImages": 14
+    },
+    {
+        "chapterNumber": "3",
+        "numImages": 15
+    },
+    {
+        "chapterNumber": "4",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "5",
+        "numImages": 14
+    },
+    {
+        "chapterNumber": "6",
+        "numImages": 14
+    },
+    {
+        "chapterNumber": "7",
+        "numImages": 14
+    },
+    {
+        "chapterNumber": "8",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "9",
+        "numImages": 9
+    },
+    {
+        "chapterNumber": "10",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "11",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "12",
+        "numImages": 14
+    },
+    {
+        "chapterNumber": "13",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "14",
+        "numImages": 14
+    },
+    {
+        "chapterNumber": "15",
+        "numImages": 14
+    },
+    {
+        "chapterNumber": "16",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "17",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "18",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "19",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "20",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "21",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "22",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "23",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "24",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "25",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "26",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "27",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "28",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "29",
+        "numImages": 15
+    },
+    {
+        "chapterNumber": "30",
+        "numImages": 15
+    },
+    {
+        "chapterNumber": "31",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "32",
+        "numImages": 15
+    },
+    {
+        "chapterNumber": "33",
+        "numImages": 14
+    },
+    {
+        "chapterNumber": "34",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "35",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "36",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "37",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "38",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "39",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "40",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "41",
+        "numImages": 14
+    },
+    {
+        "chapterNumber": "42",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "43",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "44",
+        "numImages": 15
+    },
+    {
+        "chapterNumber": "45",
+        "numImages": 15
+    },
+    {
+        "chapterNumber": "46",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "47",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "48",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "49",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "50",
+        "numImages": 14
+    },
+    {
+        "chapterNumber": "51",
+        "numImages": 14
+    },
+    {
+        "chapterNumber": "52",
+        "numImages": 14
+    },
+    {
+        "chapterNumber": "53",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "54",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "55",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "56",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "57",
+        "numImages": 14
+    },
+    {
+        "chapterNumber": "58",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "59",
+        "numImages": 14
+    },
+    {
+        "chapterNumber": "60",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "61",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "62",
+        "numImages": 17
+    },
+    {
+        "chapterNumber": "63",
+        "numImages": 17
+    },
+    {
+        "chapterNumber": "64",
+        "numImages": 27
+    },
+    {
+        "chapterNumber": "65",
+        "numImages": 24
+    },
+    {
+        "chapterNumber": "66",
+        "numImages": 27
+    },
+    {
+        "chapterNumber": "67",
+        "numImages": 22
+    },
+    {
+        "chapterNumber": "68",
+        "numImages": 31
+    },
+    {
+        "chapterNumber": "69",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "70",
+        "numImages": 23
+    },
+    {
+        "chapterNumber": "71",
+        "numImages": 26
+    },
+    {
+        "chapterNumber": "72",
+        "numImages": 31
+    },
+    {
+        "chapterNumber": "73",
+        "numImages": 21
+    },
+    {
+        "chapterNumber": "74",
+        "numImages": 24
+    },
+    {
+        "chapterNumber": "75",
+        "numImages": 22
+    },
+    {
+        "chapterNumber": "76",
+        "numImages": 22
+    },
+    {
+        "chapterNumber": "77",
+        "numImages": 23
+    },
+    {
+        "chapterNumber": "78",
+        "numImages": 24
+    },
+    {
+        "chapterNumber": "79",
+        "numImages": 24
+    },
+    {
+        "chapterNumber": "80",
+        "numImages": 31
+    },
+    {
+        "chapterNumber": "81",
+        "numImages": 24
+    },
+    {
+        "chapterNumber": "82",
+        "numImages": 9
+    },
+    {
+        "chapterNumber": "83",
+        "numImages": 26
+    },
+    {
+        "chapterNumber": "84",
+        "numImages": 31
+    },
+    {
+        "chapterNumber": "85",
+        "numImages": 30
+    },
+    {
+        "chapterNumber": "86",
+        "numImages": 32
+    },
+    {
+        "chapterNumber": "87",
+        "numImages": 25
+    },
+    {
+        "chapterNumber": "88",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "89",
+        "numImages": 14
+    },
+    {
+        "chapterNumber": "90",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "91",
+        "numImages": 28
+    },
+    {
+        "chapterNumber": "92",
+        "numImages": 30
+    },
+    {
+        "chapterNumber": "93",
+        "numImages": 27
+    },
+    {
+        "chapterNumber": "94",
+        "numImages": 26
+    },
+    {
+        "chapterNumber": "95",
+        "numImages": 27
+    },
+    {
+        "chapterNumber": "96",
+        "numImages": 36
+    },
+    {
+        "chapterNumber": "97",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "98",
+        "numImages": 43
+    },
+    {
+        "chapterNumber": "99",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "100",
+        "numImages": 14
+    },
+    {
+        "chapterNumber": "101",
+        "numImages": 32
+    },
+    {
+        "chapterNumber": "102",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "103",
+        "numImages": 27
+    },
+    {
+        "chapterNumber": "104",
+        "numImages": 32
+    },
+    {
+        "chapterNumber": "105",
+        "numImages": 22
+    },
+    {
+        "chapterNumber": "106",
+        "numImages": 24
+    },
+    {
+        "chapterNumber": "107",
+        "numImages": 23
+    },
+    {
+        "chapterNumber": "108",
+        "numImages": 26
+    },
+    {
+        "chapterNumber": "109",
+        "numImages": 23
+    },
+    {
+        "chapterNumber": "110",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "111",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "112",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "113",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "114",
+        "numImages": 14
+    },
+    {
+        "chapterNumber": "115",
+        "numImages": 9
+    },
+    {
+        "chapterNumber": "116",
+        "numImages": 23
+    },
+    {
+        "chapterNumber": "117",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "118",
+        "numImages": 20
+    },
+    {
+        "chapterNumber": "119",
+        "numImages": 21
+    },
+    {
+        "chapterNumber": "120",
+        "numImages": 19
+    },
+    {
+        "chapterNumber": "121",
+        "numImages": 30
+    },
+    {
+        "chapterNumber": "122",
+        "numImages": 24
+    },
+    {
+        "chapterNumber": "123",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "124",
+        "numImages": 25
+    },
+    {
+        "chapterNumber": "125",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "126",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "127",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "128",
+        "numImages": 31
+    },
+    {
+        "chapterNumber": "129",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "130",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "131",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "132",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "133",
+        "numImages": 25
+    },
+    {
+        "chapterNumber": "134",
+        "numImages": 25
+    },
+    {
+        "chapterNumber": "135",
+        "numImages": 30
+    },
+    {
+        "chapterNumber": "136",
+        "numImages": 14
+    },
+    {
+        "chapterNumber": "137",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "138",
+        "numImages": 29
+    },
+    {
+        "chapterNumber": "139",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "140",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "141",
+        "numImages": 24
+    },
+    {
+        "chapterNumber": "142",
+        "numImages": 22
+    },
+    {
+        "chapterNumber": "143",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "144",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "145",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "146",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "147",
+        "numImages": 12
+    }
+]
