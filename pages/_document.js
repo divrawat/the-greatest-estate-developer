33a8b1f71644bf2b06685b087307d47a21c65362
divrawat <divrawat2001@gmail>
2024-07-13 20:49:48 +0530

    your commit message

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,15 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta name="monetag" content="e73d68a75117a0f77d161f6af2ac1322" />
+
+        <Script
+          id="monetag-script"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(s,u,z,p){s.src = u, s.setAttribute('data-zone', z), p.appendChild(s);})(document.createElement('script'),'https://zovidree.com/tag.min.js',7726000,document.body||document.documentElement)`,
+          }}
+        />
+
       </Head>
       <body>
         <Main />
